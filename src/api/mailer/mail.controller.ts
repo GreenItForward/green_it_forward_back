024debb98fdaa033fc5d38bda10bec73fe45058c
Ronan Kielt <rkielt@myges.fr>
2023-06-20 19:08:56 +0200

    fix: swagger parameters for email

--- src/api/mailer/mail.controller.ts
+++ src/api/mailer/mail.controller.ts
@@ -1,8 +1,9 @@
 import { Body, ClassSerializerInterceptor, Controller, Post, Req, UseGuards, UseInterceptors } from "@nestjs/common";
 import { Request } from "express";
 import { MailService } from "./mail.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "@/api/user/auth/auth.guard";
+import { EmailDto } from "@/api/mailer/email.dto";
 
 @ApiTags('Mailer')
 @Controller('mail')
@@ -12,21 +13,24 @@ export class MailController {
     @Post('send/hi')
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    async sendHiEmail(@Body() body: { email: string, name: string }) {
+    @ApiBody({ type: EmailDto })
+    async sendHiEmail(@Body() body: EmailDto) {
         return await this.mailService.sendMail(body);
     } 
     
     @Post('send/reset-password')
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    async sendResetPasswordEmail(@Body() body: { email: string, name: string }, @Req() req: Request) {
+    @ApiBody({ type: EmailDto })
+    async sendResetPasswordEmail(@Body() body: EmailDto, @Req() req: Request) {
         return await this.mailService.sendResetPasswordEmail(body, req.headers.authorization);
     }
 
     @Post('send/confirmation')
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    async sendUserConfirmation(@Body() body: { email: string, name: string }, @Req() req: Request) {
+    @ApiBody({ type: EmailDto })
+    async sendUserConfirmation(@Body() body: EmailDto, @Req() req: Request) {
         return await this.mailService.sendUserConfirmation(body, req.headers.authorization);
     }
-}   
+}
