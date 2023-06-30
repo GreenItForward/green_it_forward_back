78aba14451e8058517e2b1a937b12216eb26ad56
James ABIB <james.bidule@gmail.com>
2023-06-30 13:11:03 +0200

    fix(auth): Update return types in auth controller and service
    - Updated the return type of the `register` method in the `AuthController` to be `Promise<TokenResponse>` instead of `Promise<string | never>`.
    - Updated the return type of the `login` method in the `AuthController` to be `Promise<TokenResponse | never>` instead of `Promise<string | never>`.
    - Updated the return type of the `register` method in the `AuthService` to be `Promise<TokenResponse>` instead of `Promise<string | never>`.
    - Updated the return type of the `login` method in the `AuthService` to be `Promise<TokenResponse>` instead of `Promise<string | never>`.

--- src/api/user/auth/auth.service.ts
+++ src/api/user/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { RegisterDto, LoginDto } from './auth.dto';
 import { AuthHelper } from './auth.helper';
 import { log } from 'console';
+import { TokenResponse } from '@/common/types/token-response.interface';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +15,7 @@ export class AuthService {
   @Inject(AuthHelper)
   private readonly helper: AuthHelper;
 
-  public async register(body: RegisterDto): Promise<string | never> {
+  public async register(body: RegisterDto): Promise<TokenResponse> {
     const { firstName, lastName, email, password }: RegisterDto = body;
     let user: User = await this.repository.findOneBy({ email });
 
@@ -30,10 +31,10 @@ export class AuthService {
     user.password = this.helper.encodePassword(password);
 
     await this.repository.save(user);
-    return this.helper.generateToken(user);
+    return {token: this.helper.generateToken(user)};
   }
 
-  public async login(body: LoginDto): Promise<string | never> {
+  public async login(body: LoginDto): Promise<TokenResponse> {
     const { email, password }: LoginDto = body;
     const user: User = await this.repository.findOneBy({ email });
 
@@ -49,9 +50,7 @@ export class AuthService {
 
     await this.repository.update(user.id, {lastLoginAt: new Date()});
 
-
-
-    return this.helper.generateToken(user);
+    return {token: this.helper.generateToken(user)};
   }
 
   public async refresh(user: User): Promise<string> {
